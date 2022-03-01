d31a6972d583267fea256ab5bef43b1dd5572137
Luis Crespo <lcrespom@gmail.com>
2022-03-01 12:21:06 +0100

    Added package.json and more detail trace

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,15 +1,17 @@
-const http = require("http");
-const os = require("os");
+const http = require('http')
+const os = require('os')
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const hostname = '127.0.0.1'
+const port = 3000
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end(`Hello from NodeJS at ${os.hostname()}`);
-});
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    let nvers = 'Node version: ' + process.version
+    let host = 'Host name: ' + os.hostname()
+    res.end(`Hello from NodeJS\n${nvers}\n${host}`)
+})
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
